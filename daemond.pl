8a2c1d9e02652b8fef5187f3d35fea0673c064bb
F.Y <fy@yiyou.org>
2015-01-15 20:37:13 +0800

    Update daemond.pl

--- daemond.pl
+++ daemond.pl
@@ -5,6 +5,8 @@ use  File::Slurp;
 use App::Daemon qw( daemonize );
 #$App::Daemon::background = 1;
 my $code=read_file('ddns.pl');
+$App::Daemon::as_user    = "root";
+$App::Daemon::as_group   = "root";
 daemonize();
 
 #	print $code;
