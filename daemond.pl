406b02d7d089f1a699133a6afcc38e3d14e00e79
F.Y <fy@yiyou.org>
2015-01-17 20:50:40 +0800

    Update daemond.pl

--- daemond.pl
+++ daemond.pl
@@ -3,6 +3,10 @@
 use strict;
 use  File::Slurp;
 use App::Daemon qw( daemonize );
+use Proc::Fork;
+#use POSIX 'WNOHANG'; 
+#$SIG{CHLD}=sub {while (waitpid(-1,WNOHANG)>0) {}};
+
 #$App::Daemon::background = 1;
 my $code=read_file('ddns.pl');
 $App::Daemon::as_user    = "root";
@@ -11,9 +15,19 @@ daemonize();
 
 #	print $code;
 while(1){
-	eval($code);
-	if($@){
-		print  $@;
+	run_fork{
+	child{ 
+		eval($code);
+		if($@){
+			print  $@;
+		}
+		exit;
 	}
+	parent{
+	#	exit;
+		my $child_pid=shift;
+		waitpid $child_pid,0;
+	}
+	};	
 	sleep 60;#暂停后再执行
 }
